77e0643bf87efeaa69339c234f39da7cf7f5b6a4
Loosineh Hartoonian <98439771+loosineh@users.noreply.github.com>
2023-12-08 08:46:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from generateKey import generate
 from encrypt import encrypt
-
+from decrypt import decrypt  
 
 def main():
     generate("Alice")
     generate("Bob")
     encrypt("message.txt", "Bob")
 
+    # Simulate transmission by copying the encrypted data to the receiver's folder
+    import shutil
+    shutil.copy("encrypted.bin", "keys/Bob/")
+
+    # Decrypt the transmitted data at the receiver's end
+    decrypt("keys/Bob/encrypted.bin", "Bob")
 
 if __name__ == "__main__":
     main()
